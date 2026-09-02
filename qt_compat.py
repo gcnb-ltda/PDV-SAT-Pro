c140a969668c69f1e73c8089496f3395ea9eb8c8
GCNB Ltda <gcnbltda@gmail.com>
2026-09-02 16:09:27 -0300

    Adiciona compatibilidade Qt para Windows x86

---
+++ qt_compat.py
@@ -0,0 +1,14 @@
+try:
+    from PySide6.QtCore import Qt
+    from PySide6.QtGui import QKeySequence, QShortcut
+    from PySide6.QtWidgets import *
+    QT_VERSION = 6
+except ImportError:
+    from PySide2.QtCore import Qt
+    from PySide2.QtGui import QKeySequence
+    from PySide2.QtWidgets import *
+    QShortcut = QShortcut
+    QT_VERSION = 5
+
+def dialog_exec(dialog):
+    return dialog.exec() if QT_VERSION == 6 else dialog.exec_()
