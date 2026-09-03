fb8cc3ba74ccf9ca4660b69a291519af9d49ca59
GCNB Ltda <gcnbltda@gmail.com>
2026-09-02 23:32:12 -0300

    Integra central de relatórios e painel gerencial

--- qt_compat.py
+++ qt_compat.py
@@ -1,10 +1,10 @@
 try:
-    from PySide6.QtCore import Qt
+    from PySide6.QtCore import Qt, QDate
     from PySide6.QtGui import QKeySequence, QShortcut
     from PySide6.QtWidgets import *
     QT_VERSION = 6
 except ImportError:
-    from PySide2.QtCore import Qt
+    from PySide2.QtCore import Qt, QDate
     from PySide2.QtGui import QKeySequence
     from PySide2.QtWidgets import *
     QShortcut = QShortcut
